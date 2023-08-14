75c5023f326655c2bf400e017a6ad04a911f3274
Namdev Arondekar <arvandekarnamdev22@gmail.com>
2023-08-14 09:58:52 +0530

    attempt4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         B b=new B();
         b.meth();
-        B b1=new B();
-        b1.meth();
+//        B b1=new B();
+        b.meth();
     }
 
     static class A{
@@ -18,5 +18,4 @@ public class Main {
             return "Method is overridden in Extendend class B";
         }
     }
-  
 }
